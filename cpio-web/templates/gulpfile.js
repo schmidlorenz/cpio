89d5d554d6bec605032a36a2e20745a7245819d4
Lorenz Schmid <schmid.lorenz@gmail.com>
2015-12-14 18:25:48 +0100

    better web frontend folder and generation structure

--- cpio-web/templates/gulpfile.js
+++ cpio-web/templates/gulpfile.js
@@ -1,45 +1,45 @@
-var gulp = require('gulp'), 
+// Load plugins
+var gulp = require('gulp'),
 	bower = require('gulp-bower'),
-	notify = require('gulp-notify') ,
-	concat = require('gulp-concat'),
+	sass = require('gulp-ruby-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	jshint = require('gulp-jshint'),
 	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
-	sass = require('gulp-ruby-sass') ,
-	autoprefixer = require('gulp-autoprefixer'),
-	minifyCss = require('gulp-minify-css');
-
+	concat = require('gulp-concat'),
+	notify = require('gulp-notify'),
+	rimraf = require('rimraf');
 
 var config = {
- 	sassSrcPath: './src/scss',
-	jsSrcPath: './src/js',
-	sassDestPath: './assets/css',
-	jsDestPath: './assets/js',
- 	bowerDir: './bower_components' 
+	htmlSrcPath:  './src/html',
+ 	sassSrcPath:  './src/scss',
+	jsSrcPath:    './src/js',
+	htmlDestPath: './out',
+	sassDestPath: './out/assets/css',
+	jsDestPath:   './out/assets/js',
+ 	bowerDir:     './bower_components' 
 }
 
 
+// Bower
 gulp.task('bower', function() { 
 	return bower()
- 		.pipe(gulp.dest(config.bowerDir)) 
+ 		.pipe(gulp.dest(config.bowerDir)) ;
 });
 
-gulp.task('fonts', function() { 
-	return gulp.src(config.bowerDir + '/bootstrap-sass/assets/fonts/bootstrap/**.*') 
-		.pipe(gulp.dest('./assets/fonts')); 
+// HTML
+gulp.task('html', function() {
+	return gulp.src(config.htmlSrcPath + '/*.html')
+		.pipe(gulp.dest(config.htmlDestPath))
+		.pipe(notify({ message: 'HTML task complete' }));
 });
 
-gulp.task('js', function() {
-	return gulp.src([config.bowerDir + '/jquery/dist/jquery.js', config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.js', config.jsSrcPath + '/*.js'])
-		.pipe(concat('main.js'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(uglify())
-		.pipe(gulp.dest(config.jsDestPath));
-});
-
+// Styles
 gulp.task('css', function() { 
 	return gulp.src(config.sassSrcPath + '/main.scss')
  		.pipe(sass({
- 			style: 'compressed',
+  			style: 'compressed',
 			'sourcemap=none': true,
  			loadPath: config.bowerDir + '/bootstrap-sass/assets/stylesheets'
  		}) )
@@ -48,8 +48,36 @@ gulp.task('css', function() { 
 			cascade: false
 		}))
 		.pipe(rename({suffix: '.min'}))
-		.pipe(minifyCss())
- 		.pipe(gulp.dest(config.sassDestPath)); 
+		.pipe(minifycss())
+ 		.pipe(gulp.dest(config.sassDestPath))
+		.pipe(notify({ message: 'Styles task complete' }));
 });
 
-  gulp.task('default', ['bower', 'fonts', 'js', 'css']);
+// Scripts
+gulp.task('js', function() {
+	return gulp.src([config.bowerDir + '/jquery/dist/jquery.js', config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.js', config.jsSrcPath + '/*.js'])
+		//.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('main.js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest(config.jsDestPath))
+		.pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// Fonts
+gulp.task('fonts', function() { 
+	return gulp.src(config.bowerDir + '/bootstrap-sass/assets/fonts/bootstrap/**.*') 
+		.pipe(gulp.dest(config.htmlDestPath + '/assets/fonts'));
+});
+
+
+// Clean
+gulp.task('clean', function(cb) {
+	return rimraf(config.htmlDestPath, cb);
+});
+
+// Default
+  gulp.task('default', ['clean'], function() {
+	gulp.start('bower', 'html', 'fonts', 'js', 'css');
+});
